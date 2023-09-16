86d818155fc2a9260b489026f36fac7389243e0b
PFLAtm <113875518+PFLAtm@users.noreply.github.com>
2023-09-16 17:25:51 +0200

    Add files via upload

---
+++ src/enemy.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "raylib.h"
+#include "Player.h"
+
+// include enemy.h and create sniper object to call DrawSniper in drawloop 
+
+
+
+
+class Sniper{
+    public:
+    float health;
+    float shootSpeed;
+    float dmg;
+    float bulletSpeed;
+    float ratio;
+    bool shouldFire;
+    bool isShooting;
+    int flipY;
+    int flipX;
+    float bulletDistance;
+    float spriteWidth;
+    float blindRadius;
+    float sightRadius;
+    float shootTimer;
+    float shootDelay;
+    Player player;
+    Vector2 bulletPos;
+    Vector2 trig;
+    Vector2 position;
+    Vector2 targetPos;
+    Rectangle sniperRec;
+    Sniper(Vector2 pos,float width,float blindR,float sightR);
+    void CalcShoot(Player player);
+    void UpdateSniper(Player player);
+    void DrawSniper(Color c,Player player);
+    
+
+};
+
+
+
